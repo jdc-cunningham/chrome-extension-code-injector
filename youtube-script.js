cfe2b56680fb4c78d8a18922ad5c79d3c74554ad
Jacob Cunningham <jcun4128@gmail.com>
2020-08-08 05:03:38 -0500

    remove old code, mostly wanted to point out also checking click coordinate

--- youtube-script.js
+++ youtube-script.js
@@ -1,29 +1,11 @@
 /**
- * note the offset units in the overlay are based on the UI as of 08/08/2020
- * as of now, this is only intended for use on Desktop, trying to actually bind to elements on page
- * as I initially did takes too long/still see the stuff that's trying to be hidden before script works
- * also note my monitor that I mainly use is at least 1920px resolution, height doesn't seem to matter.
+ * this checks what you're clicking on for home page
+ * the main intent of the YouTube aspect of this extension is to hide
+ * the recommended tiles by default. It shows them again by clicking on the page.
+ * 
+ * in order to not affect the rest of the site's funcitonality eg. searching or clicking
+ * on a playlist on the left, the code below also factors in the click coordinate.
  */
-
-// create overlay
-const customOverlay = `<div
-    id="youtube-tile-overlay"
-    style="
-        position: fixed;
-        top: 0;
-        left: 240px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: white;
-        color: #808080;
-        width: 100%;
-        height: 100%;
-        min-height: 100vh;
-        z-index: 1;
-    ">
-        <h2>Click to hide overlay</h2>
-    </div>`;
 
 window.onload = () => {
     document.body.addEventListener('click', function(e) {
